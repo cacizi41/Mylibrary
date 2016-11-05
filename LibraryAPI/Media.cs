0886f9674dc5af54fead1b244a2ac1897f6d4d31
Wenjing Qiang <qiangwenjing@gmail.com>
2016-11-04 20:43:51 -0700

    media modified

--- LibraryAPI/Media.cs
+++ LibraryAPI/Media.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace LibraryAPI
         [Key]
         public int ID { get; set; }
         public string Title { get; set; }
-        public string Gernre { get; set; }
+        public string Genre { get; set; }
         public DateTime PublishedYear { get; set; }
         public decimal Price { get; set; }
         public int Copies { get; set; }
